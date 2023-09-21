d928b50ca0e480d4059c44dd5a9dd21abf22f409
Abhyansh Shrivastava <201B013@juetguna.in>
2023-09-21 18:06:02 +0530

    Region State and City are added in sidebar

--- dashboard.py
+++ dashboard.py
@@ -19,3 +19,57 @@ if fl is not None:
 else:
     os.chdir(r"C:\Users\Abhyansh\Desktop\GIT\Py-dashboard")
     df = pd.read_csv(r"Superstore.csv", encoding="ISO-8859-1")
+
+col1, col2 = st.columns((2))
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+# Getting min and max date
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+#Create for region
+
+st.sidebar.header("Choose your filter")
+region = st.sidebar.multiselect("Pick Your Region", df["Region"].unique())
+
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+# Create for the state
+state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
+
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]
+
+# Create for city
+city = st.sidebar.multiselect("Pick the City", df3["City"].unique())
+
+# Filter the data based on Region, State and City
+if not region and not state and not city:
+    filtered_df = df
+elif not state and not city:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not city:
+    filtered_df = df[df["State"].isin(state)]
+elif state and city:
+    filtered_df = df3[df["State"].isin(state) & df3["City"].isin(city)]
+elif region and city:
+    filtered_df = df3[df["Region"].isin(region) & df3["City"].isin(city)]
+elif region and state:
+    filtered_df = df3[df["Region"].isin(region) & df3["State"].isin(state)]
+elif city:
+    filtered_df = df3[df3["City"].isin(city)]
+else:
+    filtered_df = df3[df3["Region"].isin(region) & df3["State"].isin(state) & df3["City"].isin(city)]
